89391f569c4de24468d8d52f497dc45ae49c3ef3
Viktor Breitenbuecher <vic.b@outlook.com>
2016-08-09 08:28:54 +0200

    Add new example shell script.

--- assets/setup.sh
+++ assets/setup.sh
@@ -1,16 +1,52 @@
-#!/usr/bin/env bash
+#!/bin/bash
+# Shell script to install LAMP with dependencies for running PHP applications
+# with mod_fcgi
+# -------------------------------------------------------------------------
+# Version 1.1 (August 18 2011)
+# -------------------------------------------------------------------------
+# Copyright (c) 2011 Finn Hensner <http://www.gerillafilm.se>
+# This script is licensed under GNU GPL version 2.0 or above
+# -------------------------------------------------------------------------
+apt-get update
+aptitude install apache2 apache2-suexec libapache2-mod-fcgid php5-cgi
+a2dismod php5
+a2enmod rewrite
+a2enmod suexec
+a2enmod include
+a2enmod fcgid
 
-sudo apt-get install -y build-essential
-sudo apt-get install -y mysql-server
-sudo apt-get install -y php5 php5-mysql
-sudo apt-get install -y apache2
-sudo apt-get install -y ...
+apt-get install mysql-server
+apt-get install php5-gd
+apt-get install php5-common php5-mysql
 
-sudo tee /etc/apache2/sites-available/mysite <<ENDOFFILE
-    <VirtualHost *:80>
-        Include /etc/apache2/sites-available/mysyte-common
-    </VirtualHost>
-ENDOFFILE
+sleep 1
+echo "Adding extensions and fixes to custom ini"
+cat > /etc/php5/conf.d/custom.ini << EOF
+cgi.fix_pathinfo = 1
+extension=gd2.so
+extension=pdo.so
+extension=pdo_mysql.so
+extension=php_pgsql.so
+extension=php_pdo_pgsql.so
+EOF
 
-sudo rc-update add mysql default
-sudo rc-update add apache2 default
+sleep 1
+echo "Add server name to Apache config"
+echo "ServerName 127.0.0.1" >> /etc/apache2/apache2.conf
+
+sleep 1
+echo "Installing ProFTPd server"
+apt-get purge proftpd
+apt-get install proftpd
+#jail users in their home directory
+echo -e "<Global>\nDefaultRoot ~\n</Global>" >> /etc/proftpd/proftpd.conf
+
+sleep 1
+echo "Removing default virtual host."
+rm /etc/apache2/sites-available/default
+rm /etc/apache2/sites-enabled/default-000
+
+sleep 1
+echo "Restarting apache2 and proftpd"
+service apache2 restart
+service proftpd restart
